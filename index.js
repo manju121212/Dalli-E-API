d0f570fe41efc066e64f81b8ea3fdeda177b7e3c
Manjunath <dugganimanjunath@gmail.com>
2023-10-19 20:15:11 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+ import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+const path = require('path');
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+ app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+ res.status(200).json({
+  message: 'Hello from DALL.E!',
+ });
+});
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', function (req, res) {
+ res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
+
+
+const startServer = async () => {
+ try {
+  connectDB(process.env.MONGO_URL);
+  app.listen(8080, () => console.log('Server started on port 8080'));
+ } catch (error) {
+  console.log(error);
+ }
+};
+
+startServer();
